122622981be558a9d77f1dfcbd7df205a79228a5
Parthasrikar <114384370+Parthasrikar@users.noreply.github.com>
2024-04-30 05:25:01 +0000

    added datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accoundId = 12345;
+let accountEmail = "partha@google.com";
+var accountPassword = "Asd@123";
+accountCity = "hyd";
+let state;
+
+
+// forget var use let for scope const is fixed
+
+accountEmail = "srikar@google.com";
+accountPassword = "zxcv@1234";
+accountCity = "vizag";
+
+console.table([accoundId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,15 @@
+/* 
+number - 2 power 52
+bigInt
+string - ""
+boolean
+null
+undefined
+symbol => unique
+
+object
+*/
+
+console.log(typeof 18);
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
