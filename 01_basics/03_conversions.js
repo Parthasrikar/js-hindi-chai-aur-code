9c00f150ea1f4d3b6880f8aa31b1e7fa09e45cab
Parthasrikar <114384370+Parthasrikar@users.noreply.github.com>
2024-04-30 05:58:21 +0000

    03 added

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,27 @@
+// let score = 33;
+// console.log(typeof score);
+//  let stringScore = String(score);
+//  console.log(typeof stringScore);
+//  console.log(stringScore);
+
+//  let score = "33";
+// console.log(typeof score);
+//  let stringScore = Number(score);
+//  console.log(typeof stringScore);
+//  console.log(stringScore);
+
+// "33" => number
+// "33sdd" => nan
+// true => 1    false => 0
+
+
+// boolean
+// "" false "vdfv d" true
+
+
+//  **************************** OPERATIONS *****************************************
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
