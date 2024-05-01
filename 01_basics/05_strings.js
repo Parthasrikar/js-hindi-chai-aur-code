766440c4541705b1a99728a5c81e3f2ec3761818
Parthasrikar <114384370+Parthasrikar@users.noreply.github.com>
2024-05-01 11:12:57 +0000

    added new files

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = "srikar";
+const repoCount = 50;
+
+// console.log(name + repoCount + "value");
+
+console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('srikarSR');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf("r"));
+
+const url = "https://partha.com/partha%20srikar";
+// console.log(url.replace("%20", "-"));
+console.log(url.split("/"));
---
+++ 01_basics/06_numAndMaths.js
@@ -0,0 +1,13 @@
+const score = new Number(1000);
+// console.log(score);
+// console.log(score.toFixed(2));
+// console.log(score.toLocaleString('en-IN'));
+// console.log(score.toPrecision(5));
+
+// ++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++
+
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
